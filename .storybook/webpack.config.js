bde4e2bf88364106bd3b0275977b5963387d7724
Marcio Nicolau <nicolau.3g@gmail.com>
2017-09-12 16:13:05 -0300

    update tests

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,27 +1,40 @@
-const path = require("path");
+const path = require('path');
 
-const config = {
+module.exports = {
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.css?$/,
-        loaders: ["style", "raw"],
-        include: path.resolve(__dirname, "../")
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
       },
       {
-        test: /\.json?$/,
-        loaders: ["json"],
-        include: path.resolve(__dirname, "../")
-      }
-    ]
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   externals: {
-    jsdom: "window",
-    cheerio: "window",
-    "react/lib/ExecutionEnvironment": true,
-    "react/lib/ReactContext": "window",
-    "react/addons": true
-  }
+    jsdom: 'window',
+    cheerio: 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': 'window',
+    'react/addons': true,
+  },
 };
-
-module.exports = config;
