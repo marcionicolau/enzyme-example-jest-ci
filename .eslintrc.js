fd789d189cf9e64304edaa9c1e8edf5396fa5964
Marcio Nicolau <marcionicolau@me.com>
2017-09-11 23:37:29 -0300

    update to 0.0.8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    "extends": "airbnb"
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+    serviceworker: true,
+  },
+  rules: {
+    'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
+  },
 };
