ff9ad264c4ba1f7cbef80ee74f9e614697ff453e
Marcio Nicolau <marcionicolau@me.com>
2017-09-12 02:04:29 -0300

    add knobs

--- src/Foo.stories.jsx
+++ src/Foo.stories.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 import Foo from "./Foo";
@@ -12,9 +13,23 @@ import { mount } from "enzyme";
 import expect from "expect";
 
 storiesOf("Foo", module)
+  .addDecorator(withKnobs)
   .add("Default options", () => <Foo loading info="Bar" />)
+  .add("with a button", () => (
+    <button disabled={boolean("Disabled", false)}>
+      {text("Label", "Hello Button")}
+    </button>
+  ))
+  .add("as dynamic variables", () => {
+    const name = text("Name", "Arunoda Susiripala");
+    const age = number("Age", 89);
+
+    const content = `I am ${name} and I'm ${age} years old.`;
+    return <div>{content}</div>;
+  })
   .add("Foo with interaction props", () => {
-    const story = <Foo loading={false} info="Bar" />;
+    let loaded = boolean("Loading", false);
+    const story = <Foo loading={loaded} info={text("Info", "Hello Button")} />;
 
     specs(() =>
       describe("loading setState", () => {
@@ -23,10 +38,10 @@ storiesOf("Foo", module)
           expect(output.find(".foo").length).toBe(1);
         });
 
-        it("Should have loading set to true", () => {
+        it("Should have loading set to loaded state", () => {
           const output = mount(story);
           expect(
-            output.setState({ loading: true }).find(".foo-loading").length
+            output.setState({ loading: loaded }).find(".foo-loading").length
           ).toBe(1);
         });
       })
